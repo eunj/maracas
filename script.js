df4feece96768fa1c0f66327f22adaf2be519bc1
eunj <dmswjd362@gmail.com>
2014-09-25 10:58:16 +0900

    수정

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function preload() {
 	game.load.image('drum', 'image/drum.png');
 	game.load.image('maracas', 'image/maracas.png');
 	game.load.image('guide', 'image/guide.png');
+	game.load.image('glue', 'image/glue.png');
 	
 	game.load.spritesheet('maracasui','image/maracasui.png');
 	game.load.spritesheet('brain','image/brain.png');
@@ -23,6 +24,7 @@ function create() {
 
 	game.add.sprite(0,0, 'back');
 	game.add.sprite(0,0, 'drum');
+	game.add.sprite(0,0, 'glue');
 	game.add.sprite(1722,30, 'maracas');
 	game.add.sprite(1722,30, 'guide');
 	game.add.sprite(1722,30, 'maracasui');
