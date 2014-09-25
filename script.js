837e2b40285f207b44f84803a79eb0ff78a6615d
eunj <dmswjd362@gmail.com>
2014-09-25 10:52:46 +0900

    수정

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, creat
 
 
 function preload() {
-	game.load.image('back', 'assets/back.png');
+	game.load.image('back', 'image/back.png');
+	game.load.image('drum', 'image/drum.png');
+	game.load.image('maracas', 'image/maracas.png');
+	game.load.image('guide', 'image/guide.png');
+	
+	game.load.spritesheet('maracasui','image/maracasui.png');
+	game.load.spritesheet('brain','image/brain.png');
+	game.load.spritesheet('grain','image/grain.png');
+
+	game.load.spritesheet('orain','image/orain.png');
+	game.load.spritesheet('pot','image/pot.png');
+
+	game.load.spritesheet('sprout','image/sprout.png');
 	
 
 }
@@ -10,6 +22,15 @@ function preload() {
 function create() {
 
 	game.add.sprite(0,0, 'back');
+	game.add.sprite(0,0, 'drum');
+	game.add.sprite(1722,30, 'maracas');
+	game.add.sprite(1722,30, 'guide');
+	game.add.sprite(1722,30, 'maracasui');
+	game.add.sprite(1722,30, 'grain');
+	game.add.sprite(1722,30, 'brain');
+	game.add.sprite(1722,30, 'orain');
+	game.add.sprite(1722,30, 'pot');
+	game.add.sprite(1722,30, 'sprout');
 	
 	
 }
